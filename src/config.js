1b044963e9c589950e58f9b5e813710c0b2ea827
Jay Rovacsek <jay@rovacsek.com>
2021-09-19 00:17:21 +1000

    Start rewriting some elements to reduce code paths & increase clarity

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const token = ''
+export const serverAddress = ''
+export const jellyfinUsername = ''
+export const jellyfinPassword = ''
+export const discordPrefix = '?'
+export const jellyfinAppName = 'Jellyfin Discord Music Bot'
+export const interactiveSeekBarInterval = 2000
+export const logLevel = 'info'
--- src/messagehandler.ts
+++ src/messagehandler.ts
@@ -1,5 +1,5 @@
-const CONFIG = require('../config.json')
-const Discord = require('discord.js')
+import { discordPrefix } from './config'
+import * as Discord from 'discord.js'
 const {
   checkJellyfinItemIDRegex
 } = require('./util')
@@ -14,33 +14,29 @@ const playbackmanager = require('./playbackmanager')
 const websocketHanler = require('./websockethandler')
 const discordClient = discordclientmanager.getDiscordClient()
 
-var isSummendByPlay = false
+let isSummendByPlay: boolean = false
+
+const randomNumber = (a: number, b: number): number =>
+  Math.floor(Math.random() * (Math.max(a, b) - Math.min(a, b) + 1) + Math.min(a, b))
 
 // random Color of the Jellyfin Logo Gradient
-function getRandomDiscordColor () {
-  const random = Math.random()
-  function randomNumber (b, a) {
-    return Math.floor(random * Math.pow(Math.pow((b - a), 2), 1 / 2)) + (b > a ? a : b)
-  }
+function randomColour () {
+  const gradientStart = '#AA5CC3'
+  const gradientEnd = '#00A4DC'
 
-  const GRANDIENT_START = '#AA5CC3'
-  const GRANDIENT_END = '#00A4DC'
+  const redStart = parseInt(gradientStart.slice(1, 3), 16)
+  const greenStart = parseInt(gradientStart.slice(3, 5))
+  const blueStart = parseInt(gradientStart.slice(5, 7))
 
-  let rS = GRANDIENT_START.slice(1, 3)
-  let gS = GRANDIENT_START.slice(3, 5)
-  let bS = GRANDIENT_START.slice(5, 7)
-  rS = parseInt(rS, 16)
-  gS = parseInt(gS, 16)
-  bS = parseInt(bS, 16)
+  const redEnd = parseInt(gradientEnd.slice(1, 3), 16)
+  const greenEnd = parseInt(gradientEnd.slice(3, 5))
+  const blueEnd = parseInt(gradientEnd.slice(5, 7))
 
-  let rE = GRANDIENT_END.slice(1, 3)
-  let gE = GRANDIENT_END.slice(3, 5)
-  let bE = GRANDIENT_END.slice(5, 7)
-  rE = parseInt(rE, 16)
-  gE = parseInt(gE, 16)
-  bE = parseInt(bE, 16)
+  const redValue = randomNumber(redStart, redEnd).toString(16)
+  const greenValue = randomNumber(greenStart, greenEnd).toString(16)
+  const blueValue = randomNumber(blueStart, blueEnd).toString(16)
 
-  return ('#' + ('00' + (randomNumber(rS, rE)).toString(16)).substr(-2) + ('00' + (randomNumber(gS, gE)).toString(16)).substr(-2) + ('00' + (randomNumber(bS, bE)).toString(16)).substr(-2))
+  return `#${redValue}${greenValue}${blueValue}`
 }
 
 // Song Search, return the song itemID
@@ -69,31 +65,32 @@ async function searchForItemID (searchString) {
   }
 }
 
-function summon (voiceChannel) {
-  voiceChannel.join()
-}
+const summon = (voiceChannel: Discord.VoiceChannel) => voiceChannel.join()
 
-function summonMessage (message) {
-  if (!message.member.voice.channel) {
-    message.reply('please join a voice channel to summon me!')
-  } else if (message.channel.type === 'dm') {
-    message.reply('no dms')
-  } else {
-    var desc = '**Joined Voice Channel** `'
-    desc = desc.concat(message.member.voice.channel.name).concat('`')
+const handleSummon = (message: Discord.Message): void => {
+  if (message.channel.type === 'dm') {
+    message.reply('Hi friend! I can\'t respond to direct messages, please summon me while in a voice channel')
+  }
 
+  if (message.member?.voice.channel === null) {
+    message.reply('Please join a voice channel to summon me!')
+  }
+
+  if (message.member !== null && message.member.voice.channel !== null) {
     summon(message.member.voice.channel)
-
+    const description = `<:loudspeaker:757929476993581117> **Joined Voice Channel** \`${message.member.voice.channel.name}\``
+    const colour = randomColour()
     const vcJoin = new Discord.MessageEmbed()
-      .setColor(getRandomDiscordColor())
+      .setColor(colour)
       .setTitle('Joined Channel')
       .setTimestamp()
-      .setDescription('<:loudspeaker:757929476993581117> ' + desc)
+      .setDescription(description)
+
     message.channel.send(vcJoin)
   }
 }
 
-async function playThis (message) {
+const play = async (message: string) => {
   const indexOfItemID = message.content.indexOf(CONFIG['discord-prefix'] + 'play') + (CONFIG['discord-prefix'] + 'play').length + 1
   const argument = message.content.slice(indexOfItemID)
   let items
@@ -137,122 +134,118 @@ async function addThis (message) {
   playbackmanager.addTracks(items)
 }
 
-function handleChannelMessage (message) {
-  getRandomDiscordColor()
+export const handleMessage = (message: Discord.Message) => {
+  if (message.content.startsWith(discordPrefix)) {
+    if (message.content.startsWith(CONFIG['discord-prefix'] + 'summon')) {
+      isSummendByPlay = false
 
-  if (message.content.startsWith(CONFIG['discord-prefix'] + 'summon')) {
-    isSummendByPlay = false
+      websocketHanler.openSocket()
 
-    websocketHanler.openSocket()
-
-    summonMessage(message)
-  } else if (message.content.startsWith(CONFIG['discord-prefix'] + 'disconnect')) {
-    playbackmanager.stop()
-    jellyfinClientManager.getJellyfinClient().closeWebSocket()
-    discordClient.user.client.voice.connections.forEach((element) => {
-      element.disconnect()
-    })
-    var desc = '**Left Voice Channel** `'
-    desc = desc.concat(message.member.voice.channel.name).concat('`')
-    const vcJoin = new Discord.MessageEmbed()
-      .setColor(getRandomDiscordColor())
-      .setTitle('Left Channel')
-      .setTimestamp()
-      .setDescription('<:wave:757938481585586226> ' + desc)
-    message.channel.send(vcJoin)
-  } else if ((message.content.startsWith(CONFIG['discord-prefix'] + 'pause')) || (message.content.startsWith(CONFIG['discord-prefix'] + 'resume'))) {
-    try {
-      playbackmanager.playPause()
-      const noPlay = new Discord.MessageEmbed()
-        .setColor(0xff0000)
-        .setTitle('<:play_pause:757940598106882049> ' + 'Paused/Resumed.')
-        .setTimestamp()
-      message.channel.send(noPlay)
-    } catch (error) {
-      const errorMessage = getDiscordEmbedError(error)
-      message.channel.send(errorMessage)
-    }
-  } else if (message.content.startsWith(CONFIG['discord-prefix'] + 'play')) {
-    if (discordClient.user.client.voice.connections.size < 1) {
-      summonMessage(message)
-      isSummendByPlay = true
-    }
-
-    playThis(message)
-  } else if (message.content.startsWith(CONFIG['discord-prefix'] + 'stop')) {
-    if (isSummendByPlay) {
-      if (discordClient.user.client.voice.connections.size > 0) {
-        playbackmanager.stop(discordClient.user.client.voice.connections.first())
-      }
-    } else {
+      handleSummon(message)
+    } else if (message.content.startsWith(CONFIG['discord-prefix'] + 'disconnect')) {
       playbackmanager.stop()
-    }
-  } else if (message.content.startsWith(CONFIG['discord-prefix'] + 'seek')) {
-    const indexOfArgument = message.content.indexOf(CONFIG['discord-prefix'] + 'seek') + (CONFIG['discord-prefix'] + 'seek').length + 1
-    const argument = message.content.slice(indexOfArgument)
-    try {
-      playbackmanager.seek(hmsToSeconds(argument) * 10000000)
-    } catch (error) {
-      const errorMessage = getDiscordEmbedError(error)
-      message.channel.send(errorMessage)
-    }
-  } else if (message.content.startsWith(CONFIG['discord-prefix'] + 'skip')) {
-    try {
-      playbackmanager.nextTrack()
-    } catch (error) {
-      const errorMessage = getDiscordEmbedError(error)
-      message.channel.send(errorMessage)
-    }
-  } else if (message.content.startsWith(CONFIG['discord-prefix'] + 'add')) {
-    addThis(message)
-  } else if (message.content.startsWith(CONFIG['discord-prefix'] + 'spawn')) {
-    try {
-      playbackmanager.spawnPlayMessage(message)
-    } catch (error) {
-      const errorMessage = getDiscordEmbedError(error)
-      message.channel.send(errorMessage)
-    }
-  } else if (message.content.startsWith(CONFIG['discord-prefix'] + 'help')) {
-    /* eslint-disable quotes */
-    const reply = new Discord.MessageEmbed()
-      .setColor(getRandomDiscordColor())
-      .setTitle("<:musical_note:757938541123862638> " + "Jellyfin Discord Music Bot" + " <:musical_note:757938541123862638> ")
-      .addFields({
-        name: `${CONFIG["discord-prefix"]}summon`,
-        value: "Join the channel the author of the message"
-      }, {
-        name: `${CONFIG["discord-prefix"]}disconnect`,
-        value: "Disconnect from all current Voice Channels"
-      }, {
-        name: `${CONFIG["discord-prefix"]}play`,
-        value: "Play the following item"
-      }, {
-        name: `${CONFIG["discord-prefix"]}add`,
-        value: "Add the following item to the current playlist"
-      }, {
-        name: `${CONFIG["discord-prefix"]}pause/resume`,
-        value: "Pause/Resume audio"
-      }, {
-        name: `${CONFIG["discord-prefix"]}seek`,
-        value: "Where to Seek to in seconds or MM:SS"
-      }, {
-        name: `${CONFIG["discord-prefix"]}skip`,
-        value: "Skip this Song"
-      }, {
-        name: `${CONFIG["discord-prefix"]}spawn`,
-        value: "Spawns an Interactive Play Controller"
-      }, {
-        name: `${CONFIG["discord-prefix"]}help`,
-        value: "Display this help message"
-      }, {
-        name: `GitHub`,
-        value: "Find the code for this bot at: https://github.com/KGT1/jellyfin-discord-music-bot"
+      jellyfinClientManager.getJellyfinClient().closeWebSocket()
+      discordClient.user.client.voice.connections.forEach((element) => {
+        element.disconnect()
       })
-    message.channel.send(reply)
-    /* eslint-enable quotes */
-  }
-}
+      let desc = '**Left Voice Channel** `'
+      desc = desc.concat(message.member?.voice.channel?.name).concat('`')
+      const vcJoin = new Discord.MessageEmbed()
+        .setColor(getRandomDiscordColor())
+        .setTitle('Left Channel')
+        .setTimestamp()
+        .setDescription('<:wave:757938481585586226> ' + desc)
+      message.channel.send(vcJoin)
+    } else if ((message.content.startsWith(CONFIG['discord-prefix'] + 'pause')) || (message.content.startsWith(CONFIG['discord-prefix'] + 'resume'))) {
+      try {
+        playbackmanager.playPause()
+        const noPlay = new Discord.MessageEmbed()
+          .setColor(0xff0000)
+          .setTitle('<:play_pause:757940598106882049> ' + 'Paused/Resumed.')
+          .setTimestamp()
+        message.channel.send(noPlay)
+      } catch (error) {
+        const errorMessage = getDiscordEmbedError(error)
+        message.channel.send(errorMessage)
+      }
+    } else if (message.content.startsWith(CONFIG['discord-prefix'] + 'play')) {
+      if (discordClient.user.client.voice.connections.size < 1) {
+        handleSummon(message)
+        isSummendByPlay = true
+      }
 
-module.exports = {
-  handleChannelMessage
+      play(message)
+    } else if (message.content.startsWith(CONFIG['discord-prefix'] + 'stop')) {
+      if (isSummendByPlay) {
+        if (discordClient.user.client.voice.connections.size > 0) {
+          playbackmanager.stop(discordClient.user.client.voice.connections.first())
+        }
+      } else {
+        playbackmanager.stop()
+      }
+    } else if (message.content.startsWith(CONFIG['discord-prefix'] + 'seek')) {
+      const indexOfArgument = message.content.indexOf(CONFIG['discord-prefix'] + 'seek') + (CONFIG['discord-prefix'] + 'seek').length + 1
+      const argument = message.content.slice(indexOfArgument)
+      try {
+        playbackmanager.seek(hmsToSeconds(argument) * 10000000)
+      } catch (error) {
+        const errorMessage = getDiscordEmbedError(error)
+        message.channel.send(errorMessage)
+      }
+    } else if (message.content.startsWith(CONFIG['discord-prefix'] + 'skip')) {
+      try {
+        playbackmanager.nextTrack()
+      } catch (error) {
+        const errorMessage = getDiscordEmbedError(error)
+        message.channel.send(errorMessage)
+      }
+    } else if (message.content.startsWith(CONFIG['discord-prefix'] + 'add')) {
+      addThis(message)
+    } else if (message.content.startsWith(CONFIG['discord-prefix'] + 'spawn')) {
+      try {
+        playbackmanager.spawnPlayMessage(message)
+      } catch (error) {
+        const errorMessage = getDiscordEmbedError(error)
+        message.channel.send(errorMessage)
+      }
+    } else if (message.content.startsWith(CONFIG['discord-prefix'] + 'help')) {
+      /* eslint-disable quotes */
+      const reply = new Discord.MessageEmbed()
+        .setColor(getRandomDiscordColor())
+        .setTitle("<:musical_note:757938541123862638> " + "Jellyfin Discord Music Bot" + " <:musical_note:757938541123862638> ")
+        .addFields({
+          name: `${CONFIG["discord-prefix"]}summon`,
+          value: "Join the channel the author of the message"
+        }, {
+          name: `${CONFIG["discord-prefix"]}disconnect`,
+          value: "Disconnect from all current Voice Channels"
+        }, {
+          name: `${CONFIG["discord-prefix"]}play`,
+          value: "Play the following item"
+        }, {
+          name: `${CONFIG["discord-prefix"]}add`,
+          value: "Add the following item to the current playlist"
+        }, {
+          name: `${CONFIG["discord-prefix"]}pause/resume`,
+          value: "Pause/Resume audio"
+        }, {
+          name: `${CONFIG["discord-prefix"]}seek`,
+          value: "Where to Seek to in seconds or MM:SS"
+        }, {
+          name: `${CONFIG["discord-prefix"]}skip`,
+          value: "Skip this Song"
+        }, {
+          name: `${CONFIG["discord-prefix"]}spawn`,
+          value: "Spawns an Interactive Play Controller"
+        }, {
+          name: `${CONFIG["discord-prefix"]}help`,
+          value: "Display this help message"
+        }, {
+          name: `GitHub`,
+          value: "Find the code for this bot at: https://github.com/KGT1/jellyfin-discord-music-bot"
+        })
+      message.channel.send(reply)
+      /* eslint-enable quotes */
+    }
+  }
 }
